0eb970a28b1f96acf336a3f9d7780ab69453a89f
chema969 <chema969@hotmail.com>
2018-11-26 20:19:41 +0100

    cambio dinámico

--- Practica5/FuncionesAuxiliares.hpp
+++ Practica5/FuncionesAuxiliares.hpp
@@ -1,17 +1,13 @@
 #ifndef FUNCIONES_AUXILIARES_HPP
 #define FUNCIONES_AUXILIARES_HPP
 #include <vector>
-#include <stack>
-#include "Grafo.hpp"
+
 int menu();
 
-void ViajanteDeComercio();
-
-void Viajante(Grafo &grafo,Grafo &solucion);
-
-Grafo unirConjuntos( Grafo &a, Grafo &b,std::string i,std::string j,int peso);
+void Mochila();
 
 void Cambio();
 
+int min(int i,int j);
 
 #endif
